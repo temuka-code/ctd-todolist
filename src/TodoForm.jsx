eeee1461303fccca4c993372968ade4e09ebba62
Budkas <budkas@Budkass-MacBook-Pro.local>
2025-12-16 09:37:16 -0700

    created TodoList, TodoForm components

---
+++ src/TodoForm.jsx
@@ -0,0 +1,15 @@
+function TodoForm(){
+        
+    return(
+        <form>
+            <label htmlFor="todoTitle">Todo</label>
+            <input type="text" />
+            <button>Add Todo</button>
+         </form>
+
+
+    )
+    
+}
+
+export default TodoForm
